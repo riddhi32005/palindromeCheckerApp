df10aa0dd501936f2fa5f314229d3e081f73c782
riddhi32005 <rs7219@srmist.edu.in>
2026-02-26 09:14:10 +0530

    [riddhi]added uc1

---
+++ App/src/palindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class palindromeCheckerApp {
+    public static void main (String[] args)
+    {
+        System.out.println("welcome to the palindrome checker management system"+"\nVersion : 1.0"+ "\nSystem initialized successfuly");
+
+    }
+}
